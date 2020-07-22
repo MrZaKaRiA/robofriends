e85bc698264fb2d7018a578265de0c183b91a62c
MrZaKaRiA <mrzakaria@me.com>
2020-07-22 20:44:06 +0100

    sime minor file changes

---
+++ src/robots.js
@@ -0,0 +1,74 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'shanna@melissa.tv'
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'nathan@yesenia.net'
+    },
+    {
+        id: 4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Julianne.oconner@kory.org'
+    },
+    {
+        id: 5,
+        name: 'Chelsey Dietrich',
+        username: 'Kamren',
+        email: 'Lucio_Hettinger@annie.ca'
+    },
+    {
+        id: 6,
+        name: 'Mrs. Dennis Schulist',
+        username: 'Leopoldo_Corkery',
+        email: 'karley_dach@jasper.info'
+    },
+    {
+        id: 7,
+        name: 'Kurtis Weissnat',
+        username: 'Elwyn.skiles',
+        email: 'telly.Hoeger@billy.biz'
+    },
+    {
+        id: 8,
+        name: 'Nicholas Runolfsdottir',
+        username: 'Maxime_Nienow',
+        email: 'sherwood@rosamond.me'
+    },
+    {
+        id: 9,
+        name: 'Glenna Reichert',
+        username: 'Delphine',
+        email: 'chaim_mcdermott@dana.io'
+    },
+    {
+        id: 10,
+        name: 'Clementina Dubuque',
+        username: 'Moriah.Stanton',
+        email: 'rey.padberg@karina.biz'
+    },
+    {
+        id: 11,
+        name: 'Sandor Dubuque',
+        username: 'Morsan',
+        email: 'spadberg@karina.biz'
+    },
+    {
+        id: 12,
+        name: 'Duboura Santos',
+        username: 'Stantos',
+        email: 'padberg@more.biz'
+    },
+]
